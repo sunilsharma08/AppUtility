4d01a68aa2ee0a07e7505282013d64a450f5d734
Sunil Sharma <sunilsharma.ss08@gmail.com>
2016-09-05 00:56:50 +0530

    Initial Commit

---
+++ AppUtilityExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppUtilityExample
+//
+//  Created by Apple on 05/09/16.
+//  Copyright © 2016 Sunil Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
