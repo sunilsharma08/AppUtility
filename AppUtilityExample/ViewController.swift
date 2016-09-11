c9f7e936a2a724746e3fb55bb0a73d695c5b0a4e
Sunil Sharma <sunilsharma.ss08@gmail.com>
2016-09-11 22:48:14 +0530

    AULabel example added

--- AppUtilityExample/ViewController.swift
+++ AppUtilityExample/ViewController.swift
@@ -12,7 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        auColorExamples()
+        auLabelExamples()
+    }
+    
+    func auColorExamples() {
+        //Use Color RGB Value
         self.view.backgroundColor = UIColor.init(redValue: 240, greenValue: 0, blueValue: 60)
+    }
+    
+    func auLabelExamples() {
+        //Label with edge insets
+        let auLabel = AULabel.init(frame:CGRectMake(20, 40, 50, 30))
+        auLabel.edgeInsets = UIEdgeInsetsMake(5, 5, 5, 5)
+        auLabel.text = "hello"
+        auLabel.numberOfLines = 0
+        auLabel.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(auLabel)
     }
 
     override func didReceiveMemoryWarning() {
