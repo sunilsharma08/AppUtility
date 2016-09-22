d32a83ed47f680d1373bdf1aff0a775f8ed8bee7
Sunil Sharma <sunilsharma.ss08@gmail.com>
2016-09-22 23:40:30 +0530

    Email Validation extension added

--- AppUtilityExample/AppUtility/AUString.swift
+++ AppUtilityExample/AppUtility/AUString.swift
@@ -17,4 +17,20 @@ extension String {
         return boundingBox.height
     }
     
+    public func isValidEmail(email:String,regexString:String = "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" +
+        "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+        "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" +
+        "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" +
+        "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+        "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+        "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])") -> Bool {
+        
+        return validateString(email, withRegexString: regexString)
+    }
+    
+    public func validateString(string:String,withRegexString regexString:String) -> Bool {
+        let emailTest:NSPredicate = NSPredicate(format:"SELF MATCHES %@", regexString)
+        return emailTest.evaluateWithObject(string)
+    }
+    
 }
