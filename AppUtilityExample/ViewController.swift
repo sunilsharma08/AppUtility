5da705ac6c2e2f738d3aaf8b754aad1235ea8d95
Sunil Sharma <sunilsharma.ss08@gmail.com>
2016-09-27 18:30:55 +0530

    AUDate added

--- AppUtilityExample/ViewController.swift
+++ AppUtilityExample/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         auTextFieldExample()
         addInternetCheckButton()
         auStringExamples()
+        
     }
     
     func auStringExamples() {
