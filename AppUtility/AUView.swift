d6f626f553353158085273d663002e540848b58a
Sunil Sharma <sunilsharma.ss08@gmail.com>
2017-03-02 11:44:22 +0530

    AUView added

---
+++ AppUtility/AUView.swift
@@ -0,0 +1,28 @@
+//
+//  AUView.swift
+//  AppUtilityExample
+//
+//  Created by Apple on 01/03/17.
+//  Copyright © 2017 Sunil Sharma. All rights reserved.
+//
+
+import UIKit
+
+extension UIView
+{
+    func copyView<T: UIView>() -> T? {
+        return NSKeyedUnarchiver.unarchiveObject(with: NSKeyedArchiver.archivedData(withRootObject: self)) as? T
+    }
+}
+
+/*class AUView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}*/
